8233414ccd64e07f52711fd1c57261610ddcbcd3
sw0ok <p.bialon244@gmail.com>
2023-09-25 14:33:38 +0200

    FEAT: Toast toggling

--- src/hook/context.ts
+++ src/hook/context.ts
@@ -1,9 +1,9 @@
-import React from "react";
-import ToastContainer from "../toast-container";
+import React from 'react';
+import ToastContainer from '../toast-container';
 
 export type ToastType = Pick<
-  ToastContainer,
-  "show" | "update" | "hide" | "hideAll" | "isOpen"
+    ToastContainer,
+    'show' | 'update' | 'hide' | 'hideAll' | 'isOpen' | 'toggle'
 >;
 
 const ToastContext = React.createContext({} as ToastType);
